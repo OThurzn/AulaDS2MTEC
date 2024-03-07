be0ba241cbf4acec9f5f132a20b34621e73d97a2
2º ETIM - Desenvolvimento de Sistemas <ds-etim-2@servidor.acad>
2024-03-07 14:45:45 -0300

    07.03

---
+++ CalculadoraMencao/CalculadoraMencao/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace CalculadoraMencao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.txt_mencao = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_nota3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_nota2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_nota1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_nota4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(228, 65);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(105, 54);
+            this.btn_calcular.TabIndex = 54;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // txt_mencao
+            // 
+            this.txt_mencao.ForeColor = System.Drawing.SystemColors.Window;
+            this.txt_mencao.Location = new System.Drawing.Point(228, 192);
+            this.txt_mencao.Name = "txt_mencao";
+            this.txt_mencao.ReadOnly = true;
+            this.txt_mencao.Size = new System.Drawing.Size(124, 20);
+            this.txt_mencao.TabIndex = 53;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(235, 167);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 20);
+            this.label7.TabIndex = 52;
+            this.label7.Text = "Menção:";
+            // 
+            // txt_nota3
+            // 
+            this.txt_nota3.Location = new System.Drawing.Point(27, 192);
+            this.txt_nota3.Name = "txt_nota3";
+            this.txt_nota3.Size = new System.Drawing.Size(124, 20);
+            this.txt_nota3.TabIndex = 51;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.TabIndex = 50;
+            this.label3.Text = "Nota 3:";
+            // 
+            // txt_nota2
+            // 
+            this.txt_nota2.Location = new System.Drawing.Point(27, 129);
+            this.txt_nota2.Name = "txt_nota2";
+            this.txt_nota2.Size = new System.Drawing.Size(124, 20);
+            this.txt_nota2.TabIndex = 49;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 48;
+            this.label2.Text = "Nota 2:";
+            // 
+            // txt_nota1
+            // 
+            this.txt_nota1.Location = new System.Drawing.Point(27, 65);
+            this.txt_nota1.Name = "txt_nota1";
+            this.txt_nota1.Size = new System.Drawing.Size(124, 20);
+            this.txt_nota1.TabIndex = 47;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 46;
+            this.label1.Text = "Nota 1:";
+            // 
+            // txt_nota4
+            // 
+            this.txt_nota4.Location = new System.Drawing.Point(27, 250);
+            this.txt_nota4.Name = "txt_nota4";
+            this.txt_nota4.Size = new System.Drawing.Size(124, 20);
+            this.txt_nota4.TabIndex = 56;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 20);
+            this.label4.TabIndex = 55;
+            this.label4.Text = "Nota 4:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 288);
+            this.Controls.Add(this.txt_nota4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.txt_mencao);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_nota3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_nota2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_nota1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.TextBox txt_mencao;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_nota3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_nota2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_nota1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_nota4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CalculadoraMencao/CalculadoraMencao/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraMencao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            string mencao, cor;
+            double media, nota1, nota2, nota3, nota4;
+
+            nota1 = (double.Parse(txt_nota1.Text));
+            nota2 = (double.Parse(txt_nota2.Text));
+            nota3 = (double.Parse(txt_nota3.Text));
+            nota4 = (double.Parse(txt_nota4.Text));
+            media = (nota1 + nota2 + nota3 + nota4) / 4;
+            if (media < 5){
+                mencao = "I";
+                txt_mencao.BackColor = Color.Red;
+            }
+            else if(media >=5 && media<7){
+                mencao = "R";
+                txt_mencao.BackColor = Color.Orange;
+            }
+            else if(media >=7 && media < 9){
+                mencao= "B";
+                txt_mencao.BackColor = Color.Blue;
+            }
+            else{
+                mencao = "MB";
+                txt_mencao.BackColor = Color.Blue;
+            }
+            txt_mencao.Text = "Menção "+mencao;
+        }
+    }
+}
