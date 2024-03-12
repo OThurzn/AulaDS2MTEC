e8cfdec3fa095be4eb791865f797eac6ac8c2727
2º ETIM - Desenvolvimento de Sistemas <ds-etim-2@servidor.acad>
2024-03-12 13:45:24 -0300

    12.03

---
+++ IMC/IMC/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace IMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_peso = new System.Windows.Forms.TextBox();
+            this.txt_altura = new System.Windows.Forms.TextBox();
+            this.txt_resultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(170, 53);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(93, 53);
+            this.btn_calcular.TabIndex = 0;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Peso";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Altura";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(167, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Resultado IMC";
+            // 
+            // txt_peso
+            // 
+            this.txt_peso.Location = new System.Drawing.Point(15, 70);
+            this.txt_peso.Name = "txt_peso";
+            this.txt_peso.Size = new System.Drawing.Size(100, 20);
+            this.txt_peso.TabIndex = 4;
+            // 
+            // txt_altura
+            // 
+            this.txt_altura.Location = new System.Drawing.Point(15, 128);
+            this.txt_altura.Name = "txt_altura";
+            this.txt_altura.Size = new System.Drawing.Size(100, 20);
+            this.txt_altura.TabIndex = 5;
+            // 
+            // txt_resultado
+            // 
+            this.txt_resultado.Location = new System.Drawing.Point(170, 128);
+            this.txt_resultado.Name = "txt_resultado";
+            this.txt_resultado.ReadOnly = true;
+            this.txt_resultado.Size = new System.Drawing.Size(100, 20);
+            this.txt_resultado.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 216);
+            this.Controls.Add(this.txt_resultado);
+            this.Controls.Add(this.txt_altura);
+            this.Controls.Add(this.txt_peso);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_calcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_peso;
+        private System.Windows.Forms.TextBox txt_altura;
+        private System.Windows.Forms.TextBox txt_resultado;
+    }
+}
+
---
+++ IMC/IMC/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            string resultado = "";
+            double peso, altura, imc;
+            peso = double.Parse(txt_peso.Text);
+            altura = double.Parse(txt_altura.Text);
+            imc = peso / (altura * altura);
+            if (imc < 16)
+            {
+                resultado = "Baixo peso Grau III";
+            }
+            else if (imc >= 16 && imc < 17)
+            {
+                resultado = "Baixo peso Grau II";
+            }
+            else if (imc >= 17 && imc < 18.5)
+            {
+                resultado = "Baixo peso Grau I";
+            }
+            else if (imc >= 18.5 && imc < 25)
+            {
+                resultado = "Peso saudável";
+            }
+            else if (imc >= 25 && imc < 30)
+            {
+                resultado = "Sobrepeso";
+            }
+            else if (imc >= 30 && imc < 35)
+            {
+                resultado = "Obesidade Grau I";
+            }
+            else if (imc >= 35 && imc < 40)
+            {
+                resultado = "Obesidade Grau I";
+            }
+            else if (imc >= 40)
+            {
+                resultado = "Obesidade Grau III";
+            }
+            txt_resultado.Text = resultado;
+        }
+    }
+}
